7341ae159b4ed128cc4f049f02a172e6b390c7c1
aedmark <a.edmark@gmail.com>
2025-07-09 20:25:26 -0500

    Beep Boop

--- extras/deflate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/oopis_shell
-echo "Reverting universe to a sterile, empty void..."
-cd /home/Guest
-rm -r -f docs src data reports games .secret_stuff net_practice gallery programs todo.txt README.md find_test zip_test
-echo "Sanitization complete."
